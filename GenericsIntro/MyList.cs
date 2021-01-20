49dde77f08678db70b0e16bc993c55b91a2cff6b
nidameltem95@gmail.com <nidameltem95@gmail.com>
2021-01-20 23:52:01 +0300

    20 Ocak Ders Sonu

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        //constructor
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length + 1];
+            //temparray in bütün elemankarını items e geri aktar
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1] = item;
+
+        }
+    }
+}
